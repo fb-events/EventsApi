4a9f1ab677247f2cd3fc06c14eec08445b553b9f
Dainius Kavoliunas <dainius.kavoliunas@gmail.com>
2018-01-30 14:25:07 +0100

    Retireve VUZK events

---
+++ EventsApi/Controllers/EventsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Flurl.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventsApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class EventsController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await (await @"https://graph.facebook.com/v2.11/zygeiviai/events?access_token=EAACEdEose0cBAPTMu24Va7DQ31NYO0mOcoCD5wBjhSoyZASpt6860WNcagnvpIV9RVE2ywC2wZBQI5ZC0VqYgmbniYVObNZBJZBybEnB5I2O6jxmKYOEndGTxreCZAqaeun7YaX6oSL8fZBan55GpsPklC2JMrDtoIpWuCSj2Q2JnHLrEUgKQjAlJYwqOQxZCvMZD&debug=all&format=json&method=get&pretty=0&suppress_http_code=1".GetAsync()).Content.ReadAsStringAsync());
+        }
+    }
+}
