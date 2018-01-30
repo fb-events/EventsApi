04b20ed9f62841c74992da48354a7fc50cb8dc23
Dainius Kavoliunas <dainius.kavoliunas@gmail.com>
2018-01-30 14:29:11 +0100

    Retrieve any groups public upcoming events

--- EventsApi/Controllers/EventsController.cs
+++ EventsApi/Controllers/EventsController.cs
@@ -10,10 +10,10 @@ namespace EventsApi.Controllers
     [Route("api/[controller]")]
     public class EventsController : Controller
     {
-        [HttpGet]
-        public async Task<IActionResult> Get()
+        [HttpGet("{groupId}")]
+        public async Task<IActionResult> Get(string groupId)
         {
-            return Ok(await (await @"https://graph.facebook.com/v2.11/zygeiviai/events?access_token=EAACEdEose0cBAPTMu24Va7DQ31NYO0mOcoCD5wBjhSoyZASpt6860WNcagnvpIV9RVE2ywC2wZBQI5ZC0VqYgmbniYVObNZBJZBybEnB5I2O6jxmKYOEndGTxreCZAqaeun7YaX6oSL8fZBan55GpsPklC2JMrDtoIpWuCSj2Q2JnHLrEUgKQjAlJYwqOQxZCvMZD&debug=all&format=json&method=get&pretty=0&suppress_http_code=1".GetAsync()).Content.ReadAsStringAsync());
+            return Ok(await (await $"https://graph.facebook.com/v2.11/{groupId}/events?access_token=EAACEdEose0cBAPTMu24Va7DQ31NYO0mOcoCD5wBjhSoyZASpt6860WNcagnvpIV9RVE2ywC2wZBQI5ZC0VqYgmbniYVObNZBJZBybEnB5I2O6jxmKYOEndGTxreCZAqaeun7YaX6oSL8fZBan55GpsPklC2JMrDtoIpWuCSj2Q2JnHLrEUgKQjAlJYwqOQxZCvMZD&debug=all&format=json&method=get&pretty=0&suppress_http_code=1".GetAsync()).Content.ReadAsStringAsync());
         }
     }
 }
